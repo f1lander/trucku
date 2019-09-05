4ce2bb3c68295b904286c85a0ab28e9853119f26
kenystev <kjestevez.0795@gmail.com>
2019-09-04 22:04:09 -0600

    locking versions of libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.min.css';
-import Table from './components/Table';
 import NavBar from './components/Navbar'
 import { Provider } from 'react-redux';
 import store from './store';
 
-const App = () => (
+const App: React.FC = () => (
   <Provider store={store}>
     <NavBar />
   </Provider>
