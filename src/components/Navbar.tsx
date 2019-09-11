a0bcf73696a908e0e2b9b1cfe6be8d18c9f55323
kenystev <kjestevez.0795@gmail.com>
2019-09-10 23:00:43 -0600

    Fixed issue on Navbar Burger - added Bulma-react-components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,83 +2,95 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container, Navbar } from 'react-bulma-components';
+
 import Table from './Table';
 
-const NavBar: React.FC = () => {
+interface INavbarState {
+    open?: boolean;
+}
 
-    return (
-        <Router>
-            <div className="container">
-                <nav className="navbar" role="navigation" aria-label="main navigation">
-                    <div className="navbar-brand">
-                        <a className="navbar-item" href="https://bulma.io">
-                            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-                        </a>
+class NavBar extends React.Component<INavbarState> {
+    state = {
+        open: false
+    }
 
-                        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
+    render() {
+        const { open } = this.state;
 
-                        <div id="navbarBasicExample" className="navbar-menu">
-                            <div className="navbar-start">
-                                <Link to="/" className="navbar-item">
-                                    Dashboard
-                                </Link>
-                                <Link to="/table" className="navbar-item">
-                                    Table
-                                </Link>
-                                <Link to="/clients" className="navbar-item">
-                                    Clientes
-                                </Link>
-                                <Link to="references-guides" className="navbar-item">
-                                    Guias de Remisión
-                                </Link>
+        return (
+            <Router>
+                <Container>
+                    <Navbar active={open} className="navbar" role="navigation" aria-label="main navigation">
+                        <Navbar.Brand>
+                            <Navbar.Item href="https://bulma.io">
+                                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                            </Navbar.Item>
 
-                                <div className="navbar-item has-dropdown is-hoverable">
-                                    <Link to="/#" className="navbar-link">
-                                        More
+                            <Navbar.Burger
+                                active={open}
+                                onClick={() => this.setState((state: INavbarState) => ({open: !state.open}))}
+                            />
+                        </Navbar.Brand>
+
+                            <Navbar.Menu>
+                                <div className="navbar-start">
+                                    <Link to="/" className="navbar-item">
+                                        Dashboard
+                                    </Link>
+                                    <Link to="/table" className="navbar-item">
+                                        Table
+                                    </Link>
+                                    <Link to="/clients" className="navbar-item">
+                                        Clientes
+                                    </Link>
+                                    <Link to="references-guides" className="navbar-item">
+                                        Guias de Remisión
                                     </Link>
 
-                                    <div className="navbar-dropdown">
-                                        <Link to="/#" className="navbar-item">
-                                            About
+                                    <div className="navbar-item has-dropdown is-hoverable">
+                                        <Link to="/#" className="navbar-link">
+                                            More
                                         </Link>
-                                        <Link to="/#" className="navbar-item">
-                                            Jobs
-                                        </Link>
-                                        <Link to="/#" className="navbar-item">
-                                            Contact
-                                        </Link>
-                                        <hr className="navbar-divider" />
-                                        <Link to="/#" className="navbar-item">
-                                            Report an issue
+
+                                        <div className="navbar-dropdown">
+                                            <Link to="/#" className="navbar-item">
+                                                About
                                             </Link>
+                                            <Link to="/#" className="navbar-item">
+                                                Jobs
+                                            </Link>
+                                            <Link to="/#" className="navbar-item">
+                                                Contact
+                                            </Link>
+                                            <hr className="navbar-divider" />
+                                            <Link to="/#" className="navbar-item">
+                                                Report an issue
+                                                </Link>
 
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="navbar-end">
-                                <div className="navbar-item">
-                                    <div className="buttons">
-                                        <Link to="/#" className="button is-primary">
-                                            <strong>Sign up</strong>
+                                <div className="navbar-end">
+                                    <div className="navbar-item">
+                                        <div className="buttons">
+                                            <Link to="/#" className="button is-primary">
+                                                <strong>Sign up</strong>
+                                            </Link>
+                                            <Link to="/#" className="button is-light">
+                                                Log in
                                         </Link>
-                                        <Link to="/#" className="button is-light">
-                                            Log in
-                                    </Link>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
+                            </Navbar.Menu>
+                    </Navbar>
 
-                <Route path="/table" component={Table}/>
-            </div>
-        </Router>
-    );
+                    <Route path="/table" component={Table}/>
+                </Container>
+            </Router>
+        );
+    }
 
 }
 // const mapStateToProps = state => ({
