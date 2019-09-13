c1705874853b65093546f7836a62b535e3162323
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2019-09-12 21:41:38 -0600

    adds detlify identity

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,21 +1,20 @@
-import { put, takeLatest, all } from 'redux-saga/effects';
+import { put, takeLatest, all } from "redux-saga/effects";
 
-const baseURL = 'http://localhost:3004';
+const baseURL = ".netlify/functions";
 
 function* fetchItems() {
-    const json = yield fetch(`${baseURL}/items`)
-        .then(response => response.json());
+  const json = yield fetch(`${baseURL}/table`).then(response =>
+    response.json()
+  );
 
-    console.log('JSON', json);
-    yield put({ type: "ITEMS_RECEIVED", payload: json });
-
+  console.log("JSON", json);
+  yield put({ type: "ITEMS_RECEIVED", payload: json });
 }
+
 function* actionWatcher() {
-    yield takeLatest('GET_ITEMS', fetchItems)
+  yield takeLatest("GET_ITEMS", fetchItems);
 }
 
 export default function* rootSaga() {
-    yield all([
-        actionWatcher(),
-    ]);
+  yield all([actionWatcher()]);
 }
